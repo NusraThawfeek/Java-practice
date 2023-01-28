7f7ac84d26cd6e5a0f0b6469c324a86f40d3d8fd
NusraThawfeek <nusra0408@gmail.com>
2023-01-28 22:43:13 +0530

    fibonacci added

--- fibonacci/src/com/practice/demo.java
+++ fibonacci/src/com/practice/demo.java
@@ -1,2 +1,42 @@
-package com.practice;public class demo {
+package com.practice;
+
+import java.util.Scanner;
+
+public class demo {
+
+    static int[] setFibonacci(int t){
+    int[] arr=new int[t];
+     int a=0;
+     int b=1;
+     int c;
+        for (int i=0;i<t;i++){
+            if(i==0){
+                arr[i]=a;
+            }else if(i == 1){
+                arr[i]=b;
+            }else {
+                c = a + b;
+                arr[i] = c;
+
+                a = b;
+                b = c;
+            }
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args){
+        System.out.println("enter the no of terms you need");
+        Scanner s=new Scanner(System.in);
+        int term = s.nextInt();
+        System.out.println("the term is "+term);
+
+      int[] arr1;
+        arr1 = setFibonacci(term);
+        for (int i=0;i<term;i++){
+            System.out.print(" "+arr1[i]);
+        }
+
+    }
 }
